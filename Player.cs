8e2e4c1e53d68a31e01cd6a28fb06f74d2a2ec2a
Yianni Skilitis <yskilitis@gmail.com>
2020-09-18 22:18:54 -0400

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Player
+    {
+        public int CurrentHitPoints { set; get; }
+        public int MaxHitPoints { set; get; }
+        public int Gold { set; get; }
+        public int ExperiencePoints { set; get; }
+        public int Level { set; get; }
+    }
+}
