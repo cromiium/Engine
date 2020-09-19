1555a0d37be9480a8b30774406bd5f5cdc9367b9
Yianni <yskilitis@gmail.com>
2020-09-18 22:41:41 -0400

    Added and cleaned up some classes.

--- Player.cs
+++ Player.cs
@@ -4,10 +4,9 @@ using System.Text;
 
 namespace Engine
 {
-    public class Player
+    public class Player : LivingCreature
     {
-        public int CurrentHitPoints { set; get; }
-        public int MaxHitPoints { set; get; }
+
         public int Gold { set; get; }
         public int ExperiencePoints { set; get; }
         public int Level { set; get; }
